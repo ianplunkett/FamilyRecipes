d4b5c1cbc4324b0ffe7d5d3f58b908391bcaaa44
Ian Plunkett <ian.plunkett@gmail.com>
2020-03-30 21:58:08 -0700

    Moving definitions into the setup function

--- FamilyRecipesTests/Services/FileServiceTests.swift
+++ FamilyRecipesTests/Services/FileServiceTests.swift
@@ -13,18 +13,25 @@ class FileServiceTests: XCTestCase {
 
     let singleRecipe: String = "SingleRecipe.json"
     let badRecipe: String = "BadRecipe.json"
+    var bundle: Bundle!
+    var fileService: FileService!
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        bundle = Bundle(for: type(of: self))
+        fileService = FileService(bundle: self.bundle)
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
+/*
+    func testBadFileName() {
+        let fileService:
+        XCTAssertThrowsError(<#T##expression: T##T#>)
+    }
+*/
 
     func testLoadModelFromFile() throws {
-        let bundle = Bundle(for: type(of: self))
-        let fileService: FileService = FileService(bundle: bundle)
         let recipe: Recipe = fileService.load(singleRecipe)
         XCTAssertTrue(type(of: recipe) == Recipe.self)
     }
