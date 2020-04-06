cffb718b0e558d9fff4cade5e80bbdcaef4e51ed
Ian Plunkett <ian.plunkett@gmail.com>
2020-04-05 21:34:38 -0700

    This representation is fine for now.

--- FamilyRecipes/Views/DirectionView.swift
+++ FamilyRecipes/Views/DirectionView.swift
@@ -22,7 +22,6 @@ struct DirectionView: View {
 
 struct DirectionView_Previews: PreviewProvider {
     static var previews: some View {
-        //TODO: This is messy and not resuable. Pull this from JSON that can also be used for tests
         DirectionView(direction: DirectionViewModel(direction: Direction(id: UUID(), step: 1, description: "Boiling Pasta for 5 minutes or until done")))
     }
 }
